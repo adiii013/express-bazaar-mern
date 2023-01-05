df4c67eb8d33a349f5d80565022c17375618d60b
Aditya <adidada023@gmail.com>
2023-01-06 01:57:24 +0530

    added filtering on basis of name

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,13 +1,14 @@
 const Product = require('../models/productModel');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncError = require('../middleware/catchAsyncError')
-
+const ApiFeatures = require("../utils/apiFeatures")
 
 const productController = {
 
     
     getAllProducts :catchAsyncError(async (req,res)=>{
-        const products = await Product.find();
+        const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter()
+        const products = await apiFeature.query;
         res.status(200).json({
             success:true,
             products
