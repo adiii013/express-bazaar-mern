f5404fc75531f3b6c7c3bc9c281174d13609c073
Aditya <adidada023@gmail.com>
2023-01-06 02:13:12 +0530

    completed product section

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,11 +7,14 @@ const productController = {
 
     
     getAllProducts :catchAsyncError(async (req,res)=>{
-        const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter()
+        const resultPerPage = 5;
+        const productCount = await Product.countDocuments();
+        const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage)
         const products = await apiFeature.query;
         res.status(200).json({
             success:true,
-            products
+            products,
+            productCount
         }) 
     }),
 
