100ee0a9f24d45c4d62a0509f7a23251ca313b7c
Aditya <adidada023@gmail.com>
2023-01-06 22:13:18 +0530

    added password email verification

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,4 +4,5 @@ const userController = require('../controllers/userController')
 router.route("/register").post(userController.registerUser)
 router.route("/login").post(userController.loginUser)
 router.route("/logout").get(userController.logoutUser)
+router.route("/password/forgot").post(userController.forgotPassword)
 module.exports = router
