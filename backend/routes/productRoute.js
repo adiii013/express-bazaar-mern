79775fb5481b3c6dbe7b273eaf91619493b40e07
Aditya <adidada023@gmail.com>
2023-01-06 17:52:35 +0530

    added jwt authentication

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const productController = require('../controllers/productController')
+const isAuthenticatedUser = require('../middleware/auth')
 
 const router = express.Router()
 
-router.route('/products').get(productController.getAllProducts)
+router.route('/products').get(isAuthenticatedUser,productController.getAllProducts)
 router.route('/products/new').post(productController.createProduct)
 router.route('/products/:id').put(productController.updateProduct).delete(productController.deleteProduct).get(productController.getProductDetails)
 
